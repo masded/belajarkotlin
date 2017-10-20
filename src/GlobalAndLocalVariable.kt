0da832dfeee0172742b8939a711da1d0fa7f9e2e
masded <dedydwiarfiyanto@gmail.com>
2017-10-20 20:36:32 +0700

    belajarkotlin2_v2

---
+++ src/GlobalAndLocalVariable.kt
@@ -0,0 +1,11 @@
+var fullname = "Dedy Dwi Arfiyanto" //global var
+fun display():Unit{
+    println(fullname)
+    //print("name:" + name)
+}
+
+fun main(args: Array<String>) {
+    var name = "Masded" //local var
+    println(fullname)
+    print("name:" + name)
+}
